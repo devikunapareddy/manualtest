65de0251d766073e08fa51b51ba8dcd8c44be3d6
DELL <DELL@DESKTOP-T6JDM2C>
2024-05-04 20:43:13 +0530

    this is my commit

---
+++ repository/src/gitrepository/addition.java
@@ -0,0 +1,25 @@
+package gitrepository;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=20;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("value of a is"+a);
+System.out.println("value of b is"+b);
+
+
+int c=30,d=40,temp;
+temp=c;
+c=d;
+d=temp;
+System.out.println(c);
+System.out.println(d);
+	}
+
+
+
+}
